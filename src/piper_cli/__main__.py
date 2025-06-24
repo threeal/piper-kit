3e6513b6def141d12d435042df75dfc167a68a7c
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-24 21:11:00 +0700

    feat: modify `__main__.py` to enable PiPER arm
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- src/piper_cli/__main__.py
+++ src/piper_cli/__main__.py
@@ -1,4 +1,7 @@
 import argparse
+import time
+
+from piper_sdk import C_PiperInterface_V2
 
 
 def main() -> None:
@@ -8,7 +11,13 @@ def main() -> None:
     parser.add_argument("-v", "--version", action="version", version="0.1.0")
     parser.parse_args()
 
-    print("Hello from PiPER!")
+    piper = C_PiperInterface_V2()
+    piper.ConnectPort()
+    while True:
+        piper.EnableArm(7)
+        piper.MotionCtrl_2(0x01, 0x01, 100, 0x00)
+        piper.JointCtrl(0, 0, 0, 0, 0, 0)
+        time.sleep(0.05)
 
 
 if __name__ == "__main__":
