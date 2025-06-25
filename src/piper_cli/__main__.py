8ab9c998bf4f8cb41cbea47e3aa777704a2d8ba3
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-25 14:02:10 +0700

    feat: add command for disabling arm (#12)
    * feat: add `disable` command
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    * feat: decrease joint speed and wait time when enabling/disabling arm
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    ---------
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- src/piper_cli/__main__.py
+++ src/piper_cli/__main__.py
@@ -1,7 +1,7 @@
 import argparse
 import warnings
 
-from .commands import command_enable
+from .commands import command_disable, command_enable
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module=r"^piper_sdk(\.|$)")
 
@@ -10,6 +10,12 @@ def main() -> None:
     parser = argparse.ArgumentParser(prog="piper")
     parser.add_argument("-v", "--version", action="version", version="0.1.0")
     subparsers = parser.add_subparsers(required=True)
+
+    disable_parser = subparsers.add_parser("disable", help="disable the PiPER arm")
+    disable_parser.set_defaults(func=command_disable)
+    disable_parser.add_argument(
+        "can_interface", nargs="?", default="can0", help="CAN interface to use"
+    )
 
     enable_parser = subparsers.add_parser("enable", help="enable the PiPER arm")
     enable_parser.set_defaults(func=command_enable)
