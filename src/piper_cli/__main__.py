131494e566a60c6c3ddddf7d3823ceb3319dcdab
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-25 11:13:49 +0700

    feat: add support for handling sub-commands (#6)
    * refactor: separate `command_enable` method
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    * feat: use subparsers to handle sub commands
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    ---------
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- src/piper_cli/__main__.py
+++ src/piper_cli/__main__.py
@@ -1,26 +1,21 @@
 import argparse
-import time
 import warnings
 
-from piper_sdk import C_PiperInterface_V2
+from .commands import command_enable
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module=r"^piper_sdk(\.|$)")
 
 
 def main() -> None:
-    parser = argparse.ArgumentParser(
-        prog="piper",
-    )
+    parser = argparse.ArgumentParser(prog="piper")
     parser.add_argument("-v", "--version", action="version", version="0.1.0")
-    parser.parse_args()
+    subparsers = parser.add_subparsers(required=True)
 
-    piper = C_PiperInterface_V2()
-    piper.ConnectPort()
-    while True:
-        piper.EnableArm(7)
-        piper.MotionCtrl_2(0x01, 0x01, 100, 0x00)
-        piper.JointCtrl(0, 0, 0, 0, 0, 0)
-        time.sleep(0.05)
+    enable_parser = subparsers.add_parser("enable", help="enable the PiPER arm")
+    enable_parser.set_defaults(func=command_enable)
+
+    args = parser.parse_args()
+    args.func(args)
 
 
 if __name__ == "__main__":
